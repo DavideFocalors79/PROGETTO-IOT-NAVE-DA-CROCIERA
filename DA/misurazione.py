3c12f689e18f6a813ad9363e000e01a1bc1c4df3
DavideLaghi <150442316+DavideFocalors79@users.noreply.github.com>
2026-05-14 12:29:18 +0200

    Delete DA directory

--- DA/misurazione.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def rileva_temperatura(min=10, max=40, decimali=2):
-    return round(random.uniform(min, max), decimali)
-
-def rileva_umidita(min=20, max=90, decimali=2):
-    return round(random.uniform(min, max), decimali)
